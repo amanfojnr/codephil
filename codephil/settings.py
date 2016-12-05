92ce3d78cd2d616c1b24c0dedbd789ce4143ee44
Kenneth Amanfo Junior <kenmarch@zoho.com>
2016-12-05 21:36:21 +0000

    fixed lading page

--- codephil/settings.py
+++ codephil/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g=zt!qie8@$oyw+q69xb6nk0j935m&weux@7$$1o$b@-8tym_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['amanfojnr.pythonanywhere.com',]
+ALLOWED_HOSTS = []
 
 SITE_ID = 1
 
